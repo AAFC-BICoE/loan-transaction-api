f3dadd85dd52280f6342d7770580f3b1fa73490d
Christian Gendreau <christiangendreau@gmail.com>
2022-02-04 10:14:44 -0500

    26433 Add shipment details Added shipment column and structure

---
+++ src/main/java/ca/gc/aafc/transaction/api/entities/Shipment.java
@@ -0,0 +1,73 @@
+package ca.gc.aafc.transaction.api.entities;
+
+import lombok.Builder;
+import lombok.Data;
+
+import javax.validation.Valid;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Size;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Data
+@Builder
+public class Shipment {
+
+  @Size(max = 1000)
+  private String contentRemarks;
+
+  private BigDecimal value;
+
+  // ISO 4217 currency code
+  @Size(min = 3, max = 3)
+  private String currency;
+
+  @Min(1)
+  private Integer itemCount;
+
+  private LocalDate shippedOn;
+
+  @Size (max = 50)
+  private String status;
+
+  @Size (max = 250)
+  private String packingMethod;
+
+  @Size (max = 50)
+  private String trackingNumber;
+
+  @Valid
+  private Address address;
+
+  @Size(max = 1000)
+  private String shipmentRemarks;
+
+  @Data
+  @Builder
+  public static class Address {
+    @Size (max = 150)
+    private String receiverName;
+
+    @Size (max = 150)
+    private String companyName;
+
+    @Size (max = 150)
+    private String addressLine1;
+    @Size (max = 150)
+    private String addressLine2;
+
+    @Size (max = 150)
+    private String city;
+
+    @Size (max = 150)
+    private String provinceState;
+
+    @Size (max = 50)
+    private String zipCode;
+
+    @Size (max = 50)
+    private String country;
+
+  }
+
+}
