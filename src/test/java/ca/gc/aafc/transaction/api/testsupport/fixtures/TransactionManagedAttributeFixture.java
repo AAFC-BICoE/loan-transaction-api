bd438cdf134d2223b7ac9cb1bbf1b0c1a6f75f64
Christian Gendreau <christiangendreau@gmail.com>
2022-03-11 12:14:14 -0500

    26883 Add bool to managed attribute Added bool to postgres enum + tests

--- src/test/java/ca/gc/aafc/transaction/api/testsupport/fixtures/TransactionManagedAttributeFixture.java
+++ src/test/java/ca/gc/aafc/transaction/api/testsupport/fixtures/TransactionManagedAttributeFixture.java
@@ -8,13 +8,15 @@ import ca.gc.aafc.transaction.api.testsupport.factories.MultilingualDescriptionF
 
 public final class TransactionManagedAttributeFixture {
 
+  public static final String GROUP = "group 1";
+
   private TransactionManagedAttributeFixture() {
   }
 
   public static TransactionManagedAttributeDto.TransactionManagedAttributeDtoBuilder newTransactionManagedAttribute() {
     return TransactionManagedAttributeDto.builder()
         .name(RandomStringUtils.randomAlphabetic(5))
-        .group("group")
+        .group(GROUP)
         .managedAttributeType(ManagedAttributeType.INTEGER)
         .createdBy("Created By")
         .acceptedValues(new String[] {})
