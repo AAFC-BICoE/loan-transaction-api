fda637676661ffc29bba8f12d36cb3d01616cf4e
Christian Gendreau <christiangendreau@gmail.com>
2022-04-04 09:22:13 -0400

    27028 Enable messaging TransactionService now extends MessageProducingService

--- src/main/java/ca/gc/aafc/transaction/api/service/TransactionService.java
+++ src/main/java/ca/gc/aafc/transaction/api/service/TransactionService.java
@@ -2,11 +2,13 @@ package ca.gc.aafc.transaction.api.service;
 
 import java.util.UUID;
 
+import ca.gc.aafc.dina.service.MessageProducingService;
+import ca.gc.aafc.transaction.api.dto.TransactionDto;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.SmartValidator;
 
 import ca.gc.aafc.dina.jpa.BaseDAO;
-import ca.gc.aafc.dina.service.DefaultDinaService;
 import ca.gc.aafc.transaction.api.entities.Transaction;
 import ca.gc.aafc.transaction.api.validation.TransactionManagedAttributeValueValidator;
 import ca.gc.aafc.transaction.api.validation.TransactionValidator;
@@ -14,7 +16,7 @@ import ca.gc.aafc.transaction.api.validation.TransactionValidator;
 import lombok.NonNull;
 
 @Service
-public class TransactionService extends DefaultDinaService<Transaction> {
+public class TransactionService extends MessageProducingService<Transaction> {
 
   private final TransactionValidator transactionValidator;
   private final TransactionManagedAttributeValueValidator transactionManagedAttributeValueValidator;
@@ -23,9 +25,10 @@ public class TransactionService extends DefaultDinaService<Transaction> {
     @NonNull BaseDAO baseDAO, 
     @NonNull SmartValidator smartValidator,
     @NonNull TransactionValidator transactionValidator,
-    @NonNull TransactionManagedAttributeValueValidator transactionManagedAttributeValueValidator
+    @NonNull TransactionManagedAttributeValueValidator transactionManagedAttributeValueValidator,
+    ApplicationEventPublisher eventPublisher
   ) {
-    super(baseDAO, smartValidator);
+    super(baseDAO, smartValidator, TransactionDto.TYPENAME, eventPublisher);
     this.transactionValidator = transactionValidator;
     this.transactionManagedAttributeValueValidator = transactionManagedAttributeValueValidator;
   }
