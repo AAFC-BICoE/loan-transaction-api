28e250c46e8fc75129b04cc622823854db7eb3d8
Mike Sun <mikesun197@gmail.com>
2022-05-05 09:14:50 -0400

    Support #27027 Allow to attach material-sample to
    https://redmine.biodiversity.agr.gc.ca/issues/27027
    -Fixed the github action error caused by not declaring a list properly

--- src/main/java/ca/gc/aafc/transaction/api/dto/TransactionDto.java
+++ src/main/java/ca/gc/aafc/transaction/api/dto/TransactionDto.java
@@ -77,7 +77,7 @@ public class TransactionDto extends AttributeMetaInfoProvider {
 
   @JsonApiRelation
   @JsonApiExternalRelation(type = "material-sample")
-  private ExternalRelationDto materialSample = List.of();
+  private List<ExternalRelationDto> materialSample = List.of();
 
   @JsonApiField(patchStrategy = PatchStrategy.SET)
   @Builder.Default
